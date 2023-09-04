d78ee9e158a75f35d38f5f9ef3af72efc5dd4947
212KamilBurzyński <212kamilburzynski@gmail.com>
2023-09-04 23:04:44 +0200

    321

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var myAge = 33;
+int myAge2 = 33;
+int newAge = myAge + 5;
+Console.WriteLine(newAge);
+uint newVar = uint.MinValue;
+long newVar2 = long.MaxValue;
+
+float myNumber = float.MaxValue;
+double newNumber2 = double.MinValue;
