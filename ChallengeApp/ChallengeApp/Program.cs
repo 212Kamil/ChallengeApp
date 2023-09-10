008fab01fdc934dd77c238191754a8b73a82fe50
212KamilBurzyński <212kamilburzynski@gmail.com>
2023-09-10 17:33:40 +0200

    dsa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,14 +1,73 @@
-﻿int [] grades = new int[5];
-string[] dayOfWeeks = new string[7];
-dayOfWeeks[0] = "poniedziałek";
-dayOfWeeks[1] = "wtorek";
-dayOfWeeks[2] = "środa";
-dayOfWeeks[3] = "czwartek";
-dayOfWeeks[4] = "piątek";
-dayOfWeeks[5] = "sobota";
-dayOfWeeks[6] = "niedziela";
+﻿var number = 4556;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-for(int i=0;i < dayOfWeeks.Length; i=i+1)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine(dayOfWeeks[i]);
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
+
+Console.WriteLine(counter0);
+Console.WriteLine(counter1);
+Console.WriteLine(counter2);
+Console.WriteLine(counter3);
+Console.WriteLine(counter4);
+Console.WriteLine(counter5);
+Console.WriteLine(counter6);
+Console.WriteLine(counter7);
+Console.WriteLine(counter8);
+Console.WriteLine(counter9);
